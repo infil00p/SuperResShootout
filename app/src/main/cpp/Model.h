d007e44b6b03ff1bac1177717350e1f244d5b912
Joe Bowser <jbowser@adobe.com>
2022-12-01 08:48:15 -0800

    Updating Headers

--- app/src/main/cpp/Model.h
+++ app/src/main/cpp/Model.h
@@ -1,9 +1,21 @@
-//
-// Created by Joe Bowser on 2022-11-25.
-//
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ ~ Copyright 2022 Adobe
+ ~
+ ~ Licensed under the Apache License, Version 2.0 (the "License");
+ ~ you may not use this file except in compliance with the License.
+ ~ You may obtain a copy of the License at
+ ~
+ ~     http://www.apache.org/licenses/LICENSE-2.0
+ ~
+ ~ Unless required by applicable law or agreed to in writing, software
+ ~ distributed under the License is distributed on an "AS IS" BASIS,
+ ~ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ ~ See the License for the specific language governing permissions and
+ ~ limitations under the License.
+ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
-#ifndef SUPERRESSHOOTOUT_MODEL_H
-#define SUPERRESSHOOTOUT_MODEL_H
+#ifndef SUPERRESSTATS_MODEL_H
+#define SUPERRESSTATS_MODEL_H
 
 #include <string>
 #include "rapidjson/document.h"
@@ -58,4 +70,4 @@ namespace MLStats
 }
 
 
-#endif //SUPERRESSHOOTOUT_MODEL_H
+#endif 
